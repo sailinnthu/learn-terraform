7220f6297b4d6710ef28e763d237bdc5453f5ab7
Sai Linn Thu <sltmycom@gmail.com>
2018-11-04 20:56:10 +0800

    added variables

--- resources.tf
+++ resources.tf
@@ -1,5 +1,5 @@
 resource "aws_vpc" "demo" {
-  cidr_block       = "192.168.0.0/16"
+  cidr_block       = "${var.vpc_cidr}"
   instance_tenancy = "default"
 
   tags {
---
+++ variables.tf
@@ -0,0 +1,8 @@
+variable "region" {
+    default = "ap-southeast-1"
+}
+
+variable "vpc_cidr" {
+    default = "192.168.0.0/16"
+}
+
